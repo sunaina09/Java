df47de5e77acbe63a5ceb398edb5001dd07b45a4
Sunaina <81823428+sunaina09@users.noreply.github.com>
2021-07-07 19:29:57 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample{
+
+ public static void main(String[] args)
+ {
+   System.out.println("Hello world");
+   int a=10;
+   int b=20;
+   System.out.println("The sum of the a and b is:"+(a+b));
+ }
+}
---
+++ Sample1.java
@@ -0,0 +1,13 @@
+class Sample1{
+
+  public static void main(String [] args){
+    
+    int a=6, b=3;
+
+    System.out.println("The addition of a and b is:"+(a+b));
+    System.out.println("The subtraction of a and b is:"+(a-b));
+    System.out.println("The multiplication of a and b is:"+(a*b));
+    System.out.println("The division of a and b is:"+(a/b));
+  }
+
+}
