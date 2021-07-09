eb21ebae12a617fb4fd7246ce6d98c474cf1be73
Sunaina <81823428+sunaina09@users.noreply.github.com>
2021-07-09 19:30:19 +0530

    Delete QnA4.java

--- QnA4.java
+++
@@ -1,9 +0,0 @@
-class QnA4{
-//4. Write a program in C++ to print the sum of two numbers using variables.   
-
-  public static void main (String [] args){
-    int a=20, b=15;
-    int sum=a+b;
-    System.out.println("The sum of two numbers is:"+ sum);
-  }
-}
