01d4e9b76b381711b6f071c971d68d7a7ba42e48
Sunaina <81823428+sunaina09@users.noreply.github.com>
2021-07-09 19:29:59 +0530

    Delete QnA19.java

--- QnA19.java
+++
@@ -1,9 +0,0 @@
-class QnA19{
-//19. Write a program in C++ to find the area and circumference of a circle.   
-  public static void main(String []args){
-    double r=5, pi=3.14159;
-    System.out.println("Input the radius(1/2 of diameter) of a circle :"+r);
-    System.out.println("The area of the circle is :"+(pi*r*r));
-    System.out.println("The circumference of the circle is :"+(2*pi*r));
-  }
-}
