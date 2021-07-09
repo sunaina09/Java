d2cc5594fe0b77d023aea065a45a591ca8baf0a3
Sunaina <81823428+sunaina09@users.noreply.github.com>
2021-07-09 19:30:13 +0530

    Delete QnA20.java

--- QnA20.java
+++
@@ -1,9 +0,0 @@
-class QnA20{
-//20. Write a program in C++ to convert temperature in Celsius to Fahrenheit.   
-  public static void main(String []args){
-    int tempC=35;
-    System.out.println("The temperature in Celsius :"+tempC);
-    int tempF=((9*tempC)/5+32);
-    System.out.println("The temperature in Fahrenheit : "+tempF);
-    }
-}
